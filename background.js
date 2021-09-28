389730e235229929e878ebc8eb7f3af53dc428b7
Pedro Afonso M. Nunes <pedroaf1233@gmail.com>
2021-09-28 18:45:37 +0000

    🚧 fix notification duplication

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 (() => {
+  var lastnotification = "";
+
   // Throws notification when called in content.js in L:179
   chrome.runtime.onMessage.addListener((data) => {
     if (data.type === "notification") {
-      chrome.notifications.create("", data.options);
+        // check if the notification is similar to the last one sent
+        if(lastnotification != `${data.alert_word}${data.speaker}`){
+              lastnotification = `${data.alert_word}${data.speaker}`;
+              chrome.notifications.create("", data.options);
+        }else{
+              console.log("avoided notification duplication");
+        }
     }
   });
 })();
